4136ead01c6fd6db658523a53455ce3b300bc0f6
Shehzad Hashmi <91077741+shehzadhashmis@users.noreply.github.com>
2021-09-23 12:42:20 +0500

    added comments

--- src/main/java/io/hashmis/springcloudwatch/SpringCloudWatchApplication.java
+++ src/main/java/io/hashmis/springcloudwatch/SpringCloudWatchApplication.java
@@ -3,6 +3,11 @@ package io.hashmis.springcloudwatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * A spring boot application for publishing metrics to AWS Cloud Watch.
+ *
+ * @author shehzadhashmis
+ */
 @SpringBootApplication
 public class SpringCloudWatchApplication {
 
